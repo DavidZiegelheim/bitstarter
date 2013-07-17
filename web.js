82f070b2b1ab53f31d41cd7f320463d72be521c2
DavidZiegelheim <david.ziegelheim@alum.mit.edu>
2013-07-17 02:01:24 -0400

    index.html ReadFileSync

--- web.js
+++ web.js
@@ -2,9 +2,28 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+// app.get('/', function(request, response) {
+//  response.send('Hello World 2!');
+//});
+
+var content;
+// First I want to read the file
+fs.readFileSync('./Index.html', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    content = data;
+
+    // Invoke the next step here however you like
+    console.log(content);   // Put all of the code here (not the best solution)
+    processFile();          // Or put the next step in a function and invoke it
 });
+
+function processFile() {
+    console.log(content);
+}
+
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
